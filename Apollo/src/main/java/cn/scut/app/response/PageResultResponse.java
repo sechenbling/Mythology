d22b947e379807f255c694de5840ebc4a9bd52a4
sechenbling <2040095448@qq.com>
2023-02-11 22:56:49 +0800

    add page result response type

---
+++ Apollo/src/main/java/cn/scut/app/response/PageResultResponse.java
@@ -0,0 +1,18 @@
+package cn.scut.app.response;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResultResponse<T> {
+
+  private Long    total;
+  private Integer page;
+  private Integer size;
+  private List<T> pageResult = new ArrayList<>();
+}
